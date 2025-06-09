9041305343379b5caaac90c8f9904e53489f0c13
xpr123 <3424286550@qq.com>
2025-06-09 22:51:53 +0800

    随机链表的复制

--- shu_zu/shu_zu/test.c
+++ shu_zu/shu_zu/test.c
@@ -755,10 +755,98 @@
 //}
 
 
-
-//https://leetcode.cn/problems/copy-list-with-random-pointer/description/
 //带环链表不能遍历
 
+
+//第138题 --- 随机链表的复制
+//https://leetcode.cn/problems/copy-list-with-random-pointer/description/
+//给你一个长度为n的链表,每个节点包含一个额外增加的随机指针random,
+//该指针可以指向链表中的任何节点或空节点。
+//构造这个链表的深拷贝,深拷贝应该正好由n个全新节点组成,
+//其中每个新节点的值都设为其对应的原节点的值。
+//新节点的next指针和random指针也都应指向复制链表中的新节点,
+//并使原链表和复制链表中的这些指针能够表示相同的链表状态。
+//复制链表中的指针都不应指向原链表中的节点。
+//例如,如果原链表中有X和Y两个节点,其中X.random-->Y。
+//那么在复制链表中对应的两个节点x和y,同样有x.random-->y。
+//返回复制链表的头节点。
+//用一个由n个节点组成的链表来表示输入/输出中的链表。
+//每个节点用一个[val,random_index]表示:
+//val:一个表示Node.val的整数。
+//random_index : 随机指针指向的节点索引(范围从0到n - 1);如果不指向任何节点,则为null。
+//你的代码只接受原链表的头节点head作为传入参数
+//示例1:
+//输入:head=[[7,null],[13,0],[11,4],[10,2],[1,0]]
+//输出:[[7,null],[13,0],[11,4],[10,2],[1,0]]
+//示例2:
+//输入:head=[[1, 1],[2, 1]]
+//输出:[[1,1],[2, 1]]
+//示例3:
+//输入:head = [[3, null], [3, 0], [3, null]]
+//输出:[[3, null], [3, 0], [3, null]]
+//提示:
+//0 <=n <= 1000
+//- 104<= Node.val <= 104
+//Node.random为null或指向链表中的节点。
+//struct Node 
+//{
+//    int val;
+//    struct Node *next;
+//    struct Node *random;
+//};
+//struct Node* copyRandomList(struct Node* head) 
+//{
+//    //插入拷贝节点在原节点的后面
+//    struct Node* cur = head;
+//    while (cur)
+//    {
+//        struct Node* copy = (struct Node*)malloc(sizeof(struct Node));
+//        copy->val = cur->val;
+//        struct Node* next = cur->next;
+//        //cur copy next
+//        cur->next = copy;
+//        copy->next = next;
+//        cur = next;
+//    }
+//    //处理拷贝节点的random
+//    cur = head;
+//    while (cur)
+//    {
+//        struct Node* copy = cur->next;
+//        if (cur->random == NULL)
+//        {
+//            copy->random = NULL;
+//        }
+//        else
+//        {
+//            copy->random = cur->random->next;
+//        }
+//        cur = cur->next->next;
+//    }
+//    //拷贝节点解下来链接成新链表
+//    struct Node* copyhead = NULL;
+//    struct Node* copytail = NULL;
+//    cur = head;
+//    while (cur)
+//    {
+//        struct Node* copy = cur->next;
+//        struct Node* next = copy->next;
+//        //copy尾插
+//        if (copyhead == NULL)
+//        {
+//            copyhead = copytail = copy;
+//        }
+//        else
+//        {
+//            copytail->next = copy;
+//            copytail = copytail->next;
+//        }
+//        //恢复
+//        cur->next = next;
+//        cur = next;
+//    }
+//    return copyhead;
+//}
 
 
 //第160题 --- 相交链表
