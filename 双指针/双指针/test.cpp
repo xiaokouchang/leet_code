4b1d939dee47cf4b6781d423308bc29d9b9c5715
17829285907 <3424286550@qq.com>
2025-12-10 21:58:11 +0800

    复习双指针

--- 双指针/双指针/test.cpp
+++ 双指针/双指针/test.cpp
@@ -1,4 +1,5 @@
 //283
+//移动零
 //https://leetcode.cn/problems/move-zeroes/description/
 //方法1
 //赋值
@@ -183,6 +184,40 @@
 //};
 
 
+//方法4
+//额外空间
+//class Solution {
+//public:
+//    void duplicateZeros(vector<int>& arr) {
+//        int left = 0;
+//        int n = arr.size();
+//        vector<int> arr1;      // 记录非0元素
+//        vector<int> arr2(arr); // 拷贝
+//        arr1.resize(n);
+//        int i = 0;
+//        for (left = 0; left < n; left++) {
+//            if (arr[left] != 0) {
+//                arr1[i++] = arr[left];
+//            }
+//        }
+//        i = 0;
+//        int j = 0;
+//        for (left = 0; left < n; left++) {
+//            if (arr2[j] != 0) {
+//                arr[left] = arr1[i++];
+//            }
+//            else {
+//                arr[left] = 0;
+//                if (left + 1 < n) {
+//                    left++;
+//                    arr[left] = 0;
+//                }
+//            }
+//            j++;
+//        }
+//    }
+//};
+
 
 //第202题
 //快乐数
@@ -315,5 +350,274 @@
 //            }
 //        }
 //        return count;
+//    }
+//};
+
+
+//方法2
+//排序+双指针
+//class Solution {
+//public:
+//    int triangleNumber(vector<int>& nums) {
+//        //排序降低时间复杂度
+//        sort(nums.begin(), nums.end());
+//        //双指针
+//        int ret = 0;
+//        int n = nums.size();
+//        for (int i = n - 1; i >= 2; i--)
+//        {
+//            //利用双指针快速统计符合要求的三元组的个数
+//            int left = 0;
+//            int right = i - 1;
+//            while (left < right)
+//            {
+//                if (nums[left] + nums[right] > nums[i])
+//                {
+//                    ret += right - left;
+//                    right--;
+//                }
+//                else
+//                {
+//                    left++;
+//                }
+//            }
+//        }
+//        return ret;
+//    }
+//};
+
+
+//三数之和
+//第15题
+//https://leetcode.cn/problems/3sum/description/
+//方法1
+//class Solution 
+//{
+//public:
+//    vector<vector<int>> threeSum(vector<int>& nums) 
+//    {
+//        vector<vector<int>> result;
+//        sort(nums.begin(), nums.end()); // 排序，方便去重和双指针操作
+//        int n = nums.size();
+//        for (int i = 0; i < n; i++) 
+//        {
+//            // 跳过i的重复元素
+//            if (i > 0 && nums[i] == nums[i - 1])
+//            {
+//                continue;
+//            }
+//            int left = i + 1; // 左指针
+//            int right = n - 1; // 右指针
+//            while (left < right) 
+//            {
+//                int sum = nums[i] + nums[left] + nums[right];
+//                if (sum == 0) 
+//                {
+//                    // 找到符合条件的三元组
+//                    result.push_back({ nums[i], nums[left], nums[right] });
+//                    // 跳过left的重复元素
+//                    while (left < right && nums[left] == nums[left + 1])
+//                    {
+//                        left++;
+//                    }
+//                    // 跳过right的重复元素
+//                    while (left < right && nums[right] == nums[right - 1])
+//                    {
+//                        right--;
+//                    }
+//                    // 移动指针继续寻找
+//                    left++;
+//                    right--;
+//                }
+//                else if (sum < 0) 
+//                {
+//                    // 和太小，左指针右移增大和
+//                    left++;
+//                }
+//                else 
+//                {
+//                    // 和太大，右指针左移减小和
+//                    right--;
+//                }
+//            }
+//        }
+//        return result;
+//    }
+//};
+
+
+//方法2
+//class Solution {
+//public:
+//    vector<vector<int>> threeSum(vector<int>& nums) {
+//        vector<vector<int>> result;
+//        // 排序
+//        sort(nums.begin(), nums.end());
+//        // 双指针
+//        int n = nums.size();
+//        for (size_t i = 0; i < n - 2;) {
+//            if (nums[i] > 0) {
+//                // 固定的第1个数大于0,所有的数字之和一定大于0
+//                break;
+//            }
+//            int left = i + 1;
+//            int right = n - 1;
+//            int target = -nums[i];
+//            while (left < right) {
+//                int sum = nums[left] + nums[right];
+//                if (sum > target) {
+//                    right--;
+//                }
+//                else if (sum < target) {
+//                    left++;
+//                }
+//                else {
+//                    result.push_back({ nums[i], nums[left], nums[right] });
+//                    left++;
+//                    right--;
+//                    while (left < right && nums[left] == nums[left - 1]) {
+//                        left++;
+//                    }
+//                    while (left < right && nums[right] == nums[right + 1]) {
+//                        right--;
+//                    }
+//                }
+//            }
+//            i++;
+//            while (i < n && i>0 && nums[i] == nums[i - 1])
+//            {
+//                i++;
+//            }
+//        }
+//        return result;
+//    }
+//};
+
+
+//四数之和
+//方法1
+//排序+双指针(类似于三数之和)
+//class Solution 
+//{
+//public:
+//    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+//        vector<vector<int>> vec;
+//        // 1. 排序
+//        sort(nums.begin(), nums.end());
+//
+//        // 利用双指针解决问题
+//        int n = nums.size();
+//        for (int i = 0; i < n - 3;) {
+//
+//            for (int j = i + 1; j < n - 2;) {
+//                int left = j + 1;
+//                int right = n - 1;
+//                long long aim = (long long)target - nums[i] - nums[j];
+//                while (left < right) {
+//                    int sum = nums[left] + nums[right];
+//                    if (sum < aim) {
+//                        left++;
+//                    }
+//                    else if (sum > aim) {
+//                        right--;
+//                    }
+//                    else {
+//                        vec.push_back(
+//                            { nums[i], nums[j], nums[left++], nums[right--] });
+//                        while (left < right && nums[left] == nums[left - 1]) {
+//                            left++;
+//                        }
+//                        while (left < right && nums[right] == nums[right + 1]) {
+//                            right--;
+//                        }
+//                    }
+//                }
+//                j++;
+//                while (j < n - 2 && nums[j] == nums[j - 1]) {
+//                    j++;
+//                }
+//            }
+//            i++;
+//            while (i < n - 3 && nums[i] == nums[i - 1]) {
+//                i++;
+//            }
+//        }
+//        return vec;
+//    }
+//};
+
+
+//方法2
+//极致优化
+//class Solution 
+//{
+//public:
+//    vector<vector<int>> fourSum(vector<int>& nums, int target) 
+// {
+//        vector<vector<int>> result;
+//        int n = nums.size();
+//        // 边界条件：数组长度不足4时直接返回空
+//        if (n < 4) return result;
+//
+//        // 排序是双指针的前提
+//        sort(nums.begin(), nums.end());
+//
+//        // 第一层循环：固定第一个数
+//        for (int i = 0; i <= n - 4; ++i) {
+//            // 去重：跳过与前一个数重复的情况
+//            if (i > 0 && nums[i] == nums[i - 1]) continue;
+//
+//            // 剪枝1：当前i的最小四数和 > target，后续更大，直接break
+//            long long min_sum_i = (long long)nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3];
+//            if (min_sum_i > target) break;
+//
+//            // 剪枝2：当前i的最大四数和 < target，当前i无解，continue
+//            long long max_sum_i = (long long)nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1];
+//            if (max_sum_i < target) continue;
+//
+//            // 第二层循环：固定第二个数
+//            for (int j = i + 1; j <= n - 3; ++j) {
+//                // 去重：跳过与前一个数重复的情况（注意j > i+1）
+//                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+//
+//                // 剪枝3：当前i+j的最小四数和 > target，break
+//                long long min_sum_j = (long long)nums[i] + nums[j] + nums[j + 1] + nums[j + 2];
+//                if (min_sum_j > target) break;
+//
+//                // 剪枝4：当前i+j的最大四数和 < target，continue
+//                long long max_sum_j = (long long)nums[i] + nums[j] + nums[n - 2] + nums[n - 1];
+//                if (max_sum_j < target) continue;
+//
+//                // 双指针：第三、第四个数
+//                int left = j + 1, right = n - 1;
+//                while (left < right) {
+//                    // 用long long避免int溢出
+//                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+//
+//                    if (sum == target) {
+//                        // 找到符合条件的四元组
+//                        result.push_back({ nums[i], nums[j], nums[left], nums[right] });
+//
+//                        // 去重：跳过left重复值
+//                        while (left < right && nums[left] == nums[left + 1]) ++left;
+//                        // 去重：跳过right重复值
+//                        while (left < right && nums[right] == nums[right - 1]) --right;
+//
+//                        // 移动指针找下一组
+//                        ++left;
+//                        --right;
+//                    }
+//                    else if (sum < target) {
+//                        // 和太小，左指针右移增大和
+//                        ++left;
+//                    }
+//                    else {
+//                        // 和太大，右指针左移减小和
+//                        --right;
+//                    }
+//                }
+//            }
+//        }
+//        return result;
 //    }
 //};
