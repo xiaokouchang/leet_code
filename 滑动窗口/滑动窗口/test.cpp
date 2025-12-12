9018df0e948e7c5f9f4bc5d6fdf4419d445ce89d
17829285907 <3424286550@qq.com>
2025-12-12 22:00:27 +0800

    最大连续1的个数

--- 滑动窗口/滑动窗口/test.cpp
+++ 滑动窗口/滑动窗口/test.cpp
@@ -32,3 +32,36 @@
 //    }
 //};
 
+
+//第1004题
+//最大连续1的个数
+//https://leetcode.cn/problems/max-consecutive-ones-iii/
+//class Solution {
+//public:
+//    int longestOnes(vector<int>& nums, int k) {
+//        int n = nums.size();
+//        int left = 0;          // 窗口左边界
+//        int zeroCount = 0;     // 窗口内0的数量
+//        int maxLen = 0;        // 最大连续1的长度
+//
+//        for (int right = 0; right < n; ++right) {
+//            // 右指针遇到0，增加0的计数
+//            if (nums[right] == 0) {
+//                zeroCount++;
+//            }
+//
+//            // 当0的数量超过k时，移动左指针缩小窗口
+//            while (zeroCount > k) {
+//                if (nums[left] == 0) {
+//                    zeroCount--;
+//                }
+//                left++;
+//            }
+//
+//            // 更新最大窗口长度
+//            maxLen = max(maxLen, right - left + 1);
+//        }
+//
+//        return maxLen;
+//    }
+//};
