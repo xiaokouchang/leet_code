2ce9dcef1814a694a82ffd6313508d0faaf59a28
17829285907 <3424286550@qq.com>
2026-04-02 20:54:52 +0800

    四数之和

--- 双指针/双指针/test.cpp
+++ 双指针/双指针/test.cpp
@@ -495,6 +495,7 @@
 //                continue;
 //            }
 //            k = i + 2;
+//			  // 2 2 3 4
 //            for (int j = i + 1; j < n - 1; j++)
 //            {
 //                while (k<n && nums[i] + nums[j]>nums[k])
@@ -712,7 +713,7 @@
 
 
 //四数之和
-//
+//https://leetcode.cn/problems/4sum/
 //方法1
 //排序+双指针(类似于三数之和)
 //class Solution 
@@ -978,5 +979,61 @@
 //            }
 //        }
 //        return quadruplets;
+//    }
+//};
+
+
+//方法5
+//class Solution 
+//{
+//public:
+//    vector<vector<int>> fourSum(vector<int>& nums, int target) 
+//{
+//        int n = nums.size();
+//        vector<vector<int>> result;
+//        int i = 0;
+//        sort(nums.begin(), nums.end());
+//        for (i = 0; i < n - 3; i++) {
+//            if (i > 0 && nums[i] == nums[i - 1]) 
+//            {
+//                continue;
+//            }
+//            for (int j = i + 1; j < n - 2; j++) 
+//            {
+//                if (j > i + 1 && nums[j] == nums[j - 1]) 
+//                {
+//                    continue;
+//                }
+//                int left = j + 1;
+//                int right = n - 1;
+//                while (left < right)
+//                {
+//                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+//                    if (sum == target)
+//                    {
+//                        result.push_back({ nums[i],nums[j],nums[left],nums[right] });
+//                        while (left < right && nums[left] == nums[left + 1])
+//                        {
+//                            left++;
+//                        }
+//                        while (left < right && nums[right] == nums[right - 1])
+//                        {
+//                            right--;
+//                        }
+//                        left++;
+//                        right--;
+//                    }
+//                    else if (sum < target)
+//                    {
+//                        left++;
+//                    }
+//                    else
+//                    {
+//                        right--;
+//                    }
+//                }
+//            }
+//        }
+//        return result;
 //    }
 //};
